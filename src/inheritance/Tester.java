374eae536fbceb5cc6049343d813f063ced24a1a
uthman15 <uthmanmusleh@gmail.com>
2022-04-16 14:56:34 -0500

    inheritance introduction

---
+++ src/inheritance/Tester.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Tester extends Person {
+
+    public Tester(){
+
+    }
+
+    public Tester(String fName, String lName, int age, String gender){
+        super(fName, lName, age, gender);
+    }
+
+    public void code(){
+        System.out.println("Tester code");
+    }
+}
